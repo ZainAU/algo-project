ed4ea8955234ff122f1bfa5f55f866ea26923f54
Zain Ahmed Usmani <46748807+ZainAU@users.noreply.github.com>
2023-04-11 10:49:43 +0500

    removed chatGPT's dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -13,43 +13,6 @@
 import heapq
 
 
-def dijkstra(graph, start):
-    """
-    Find the shortest path from a given start node to all other nodes in the graph using Dijkstra's algorithm.
-
-    graph: A dictionary representing the graph, where the keys are nodes and the values are dictionaries 
-           of the form {neighbor: cost}. The 'cost' represents the cost of the edge between the node and its neighbor.
-    start: The node to start the search from.
-
-    Returns:
-    A dictionary where the keys are the nodes and the values are the shortest path to that node from the start node.
-    """
-
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-
-    pq = [(0, start)]
-
-    while pq:
-        current_distance, current_node = heapq.heappop(pq)
-
-        # If we have already visited this node with a lower distance, skip it
-        if current_distance > distances[current_node]:
-            continue
-
-        # print(current_node)
-        # print(graph[current_node])
-
-        for neighbor, cost in graph[current_node].items():
-            # print(neighbor, cost)
-            distance = current_distance + cost
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
-
-    return distances
-
-
 def dijkstra_with_path(graph, start):
     visited = set()
     distances = {v: float('inf') for v in graph}
